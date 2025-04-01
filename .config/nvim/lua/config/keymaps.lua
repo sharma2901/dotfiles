df5ff563c12a22af6ccd0e227f052f852ccc9d07
Suryakant Sharma <suryasharma2901@gmail.com>
2025-04-02 00:51:39 +0530

    adds two slash plugin and some keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
-vim.keymap.del({ "i", "n", "s", "x" }, "<C-S>")
+-- vim.keymap.del({ "i", "n", "s", "x" }, "<C-S>")
 vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
-vim.keymap.set({ "i", "n", "s", "x" }, ";;", "<Cmd>w<CR><Esc>", { desc = "Save file and go to normal mode" })
+vim.keymap.set({ "i", "n", "s", "x" }, ";;", "<Cmd>w<CR><Esc>", { desc = "Save File" })
+vim.keymap.set({ "i" }, ";'", "<Esc>", { desc = "ESC (go to normal mode)" })
+vim.keymap.set({ "n" }, "<space>r", "<Cmd>source %<CR>", { desc = "Reload config" })
+vim.keymap.set("n", "<C-k>", "<cmd>TwoslashQueriesInspect<CR>", { desc = "Insert TwoSlash comment" })
