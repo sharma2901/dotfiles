88f1034ff127021713fc367e4f2c1b1852514e11
Suryakant Sharma <suryasharma2901@gmail.com>
2025-04-01 21:41:50 +0530

    fonts changed for wezterm and neovim

--- .config/nvim/lua/plugins/extend-colorscheme.lua
+++ .config/nvim/lua/plugins/extend-colorscheme.lua
@@ -5,6 +5,18 @@ return {
     styles = {
       sidebars = "transparent",
       floats = "transparent",
+      comments = { bold = true },
+      properties = { bold = true },
+      functions = { bold = true },
+      keywords = { italic = true },
+      operators = { bold = true },
+      conditionals = { italic = true },
+      loops = { italic = true },
+      booleans = { bold = true, italic = true },
+      numbers = {},
+      types = {},
+      strings = {},
+      variables = {},
     },
   },
 }
